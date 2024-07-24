acaa503aafe6425718ffb6b37ac15ddf94447983
Daniil Shaulov <dshaulov@outlook.com>
2024-07-24 02:39:53 +0200

    Add model for profile

--- project_tracker/project/models.py
+++ project_tracker/project/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True)
+
+    def __str__(self):
+        return self.user.username
