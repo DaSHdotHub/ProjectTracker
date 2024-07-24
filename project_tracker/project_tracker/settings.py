7c2340f368142f638a1690305ee821d9c0b5f33b
Daniil Shaulov <dshaulov@outlook.com>
2024-07-24 10:55:35 +0200

    Add Email setting to settings.py

--- project_tracker/project_tracker/settings.py
+++ project_tracker/project_tracker/settings.py
@@ -117,6 +117,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
