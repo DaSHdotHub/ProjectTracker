53c490520bc9e4c1c74b834d5b2320bc4f2fd2b3
Daniil Shaulov <dshaulov@outlook.com>
2024-07-30 03:58:56 +0200

    PROFILES: Remove debug points, add dashboard button

--- profiles/views.py
+++ profiles/views.py
@@ -19,48 +19,49 @@ class ProfileViewSet(viewsets.ModelViewSet):
 Profile page interaction
 """
 
+
 @login_required
 def profile(request):
     user = request.user
     profile, created = Profile.objects.get_or_create(user=user)
 
     if request.method == "POST":
-        print("POST request received")
-        print(f"request.FILES: {request.FILES}")
         user_form = UserForm(request.POST, instance=user)
         profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
-        
+
         if user_form.is_valid() and profile_form.is_valid():
-            print("Forms are valid")
             user_form.save()
             profile = profile_form.save(commit=False)
+            profile_updated = False
 
-            if 'profile_picture' in request.FILES:
-                print("Profile picture found in request.FILES")
-                profile_picture_url = upload_profile_picture(request.FILES['profile_picture'])
+            if "profile_picture" in request.FILES:
+                profile_picture_url = upload_profile_picture(
+                    request.FILES["profile_picture"]
+                )
                 profile.profile_picture = profile_picture_url
-                print(f"Cloudinary URL: {profile_picture_url}")
-            
+
             profile.save()
-            
+
             if user_form.cleaned_data.get("password1"):
                 user.set_password(user_form.cleaned_data["password1"])
                 user.save()
                 update_session_auth_hash(request, user)
-            
+
             messages.success(request, "Your profile was successfully updated!")
-            return redirect("dashboard")
+            # Set a session variable to flag that the profile was updated
+            profile_updated = True
+            request.session["profile_updated"] = True
+            return redirect("profile")
         else:
-            print("Forms are invalid")
-            print(f"User form errors: {user_form.errors}")
-            print(f"Profile form errors: {profile_form.errors}")
             messages.error(request, "Please correct the errors below.")
     else:
         user_form = UserForm(instance=user)
         profile_form = ProfileForm(instance=profile)
 
     profile_picture_url = profile.profile_picture if profile.profile_picture else None
-    print(f"Retrieved profile picture URL: {profile_picture_url}")
+
+    # Check if the profile was updated in this session
+    show_dashboard_button = request.session.get("profile_updated", False)
 
     return render(
         request,
@@ -69,6 +70,7 @@ def profile(request):
             "user_form": user_form,
             "profile_form": profile_form,
             "profile_picture_url": profile_picture_url,
+            "show_dashboard_button": show_dashboard_button,
         },
     )
 
