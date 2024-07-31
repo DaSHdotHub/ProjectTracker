d8b53a2cce88e9c44ed6321df79e50c5f615cf4a
Daniil Shaulov <dshaulov@outlook.com>
2024-07-31 06:59:29 +0200

    format

--- project/urls.py
+++ project/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path("edit-project/<int:project_id>/", edit_project, name="edit_project"),
     path("change-task-status/", change_task_status, name="change_task_status"),
     path("delete-task/", delete_task, name="delete_task"),
-    path('project/delete/', delete_project, name='delete_project'),
+    path("project/delete/", delete_project, name="delete_project"),
     path("api/", include(router.urls)),
 ]
