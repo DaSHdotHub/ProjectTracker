080b1a1e86bea474f10b6ed6df99768ba56a8d20
Daniil Shaulov <dshaulov@outlook.com>
2024-07-31 06:18:45 +0200

    Add custom filter to attach attributes to froms

--- project/templatetags/project_filters.py
+++ project/templatetags/project_filters.py
@@ -13,3 +13,9 @@ def truncate_chars(value, max_length):
             truncated = truncated.rsplit(' ', 1)[0] + '...'
         return truncated
     return value
+
+# Custom attribute filter to add attributes to a form field
+@register.filter(name='attr')
+def attr(field, attr_args):
+    attr_key, attr_value = attr_args.split(':')
+    return field.as_widget(attrs={attr_key: attr_value})
